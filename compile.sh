4af7b63b282e7a0b7679a4b50c64b3de0214c4ab
Hector Joao Rivera Verduzco <hectorr@amd-server.inf.ethz.ch>
2023-04-13 17:52:24 +0200

    added temporal sompile script

---
+++ compile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Temporal script to compile all in amd-server machine
+
+# Compile elina
+cd /local/home/hectorr/repo/clam/build/crab/elina-prefix/src/elina
+make ELINA_PREFIX=/local/home/hectorr/repo/clam/build/run/elina
+make install ELINA_PREFIX=/local/home/hectorr/repo/clam/build/run/elina
+
+# Compile Clam and crab
+cd /local/home/hectorr/repo/clam/build
+make -j20
+make install
+
+# Add elina lib binaries to correct directory (not sure why this is not done by make install)
+cp -r /local/home/hectorr/repo/clam/install/elina/lib/. /local/home/hectorr/repo/clam/install/lib
